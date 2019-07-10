5145d760b3ca379e2c33fd28b745e84bacb9602b
viloria <viloria_f@yahoo.com.mx>
2019-07-09 20:07:32 -0500

    57. Optimizando el código para obtener la dirección y coordenadas

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const axios = require('axios');
+//const axios = require('axios');
+const lugar = require('./lugar/lugar');
+
 const argv = require('yargs').options({
     direccion: {
         alias: 'd',
@@ -6,18 +8,24 @@ const argv = require('yargs').options({
         demand: true
     }
 }).argv;
-console.log(argv.direccion);
-const encodeUlr = encodeURI(argv.direccion);
-console.log(encodeUlr);
 
-const instance = axios.create({
-    baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${ encodeUlr }`,
-    //timeout: 1000,
-    headers: { 'X-RapidAPI-Key': '7b4b343dadmshdb19b66dce59f11p1a42b8jsn3605dd9cfa35' }
-});
-instance.get()
-    .then((resp) => {
-        console.log(resp.data.Results[0]);
-    }).catch((err) => {
-        console.log('ERROR !!!!', err);
-    });
+lugar.getLugarLatLng(argv.direccion)
+    .then(console.log);
+
+//argv.direccion
+
+// console.log(argv.direccion);
+// const encodeUlr = encodeURI(argv.direccion);
+// console.log(encodeUlr);
+
+// const instance = axios.create({
+//     baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${ encodeUlr }`,
+//     //timeout: 1000,
+//     headers: { 'X-RapidAPI-Key': '7b4b343dadmshdb19b66dce59f11p1a42b8jsn3605dd9cfa35' }
+// });
+// instance.get()
+//     .then((resp) => {
+//         console.log(resp.data.Results[0]);
+//     }).catch((err) => {
+//         console.log('ERROR !!!!', err);
+//     });
---
+++ lugar/lugar.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+
+const getLugarLatLng = async(dir) => {
+    const encodeUlr = encodeURI(dir);
+
+    const instance = axios.create({
+        baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${ encodeUlr }`,
+        //timeout: 1000,
+        headers: { 'X-RapidAPI-Key': '7b4b343dadmshdb19b66dce59f11p1a42b8jsn3605dd9cfa35' }
+    });
+
+    const resp = await instance.get();
+
+    if (resp.data.Results.length === 0) {
+        throw new Error(`No hay resultados para la direccion ${ dir }`);
+    }
+    const data = resp.data.Results[0];
+    const direccion = data.name;
+    const lat = data.lat;
+    const lng = data.lon;
+
+    return {
+        direccion,
+        lat,
+        lng
+    }
+}
+module.exports = {
+    getLugarLatLng
+}
